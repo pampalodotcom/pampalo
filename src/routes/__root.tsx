545b679d10ed6751654f7f7ada509ee69fd5edd3
hooperben <s4394947@gmail.com>
2026-05-14 13:16:04 +1000

    chore: boilerplate TSS/Convex app

---
+++ src/routes/__root.tsx
@@ -0,0 +1,52 @@
+import { type QueryClient } from '@tanstack/react-query'
+import {
+  Outlet,
+  Scripts,
+  HeadContent,
+  createRootRouteWithContext,
+} from '@tanstack/react-router'
+import * as React from 'react'
+import appCss from '../styles.css?url'
+
+export const Route = createRootRouteWithContext<{
+  queryClient: QueryClient
+}>()({
+  head: () => ({
+    meta: [
+      {
+        charSet: 'utf-8',
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1',
+      },
+      {
+        title: 'TanStack Start Starter',
+      },
+    ],
+    links: [{ rel: 'stylesheet', href: appCss }],
+  }),
+  component: RootComponent,
+})
+
+function RootComponent() {
+  return (
+    <RootDocument>
+      <Outlet />
+    </RootDocument>
+  )
+}
+
+function RootDocument({ children }: { children: React.ReactNode }) {
+  return (
+    <html>
+      <head>
+        <HeadContent />
+      </head>
+      <body suppressHydrationWarning>
+        {children}
+        <Scripts />
+      </body>
+    </html>
+  )
+}
